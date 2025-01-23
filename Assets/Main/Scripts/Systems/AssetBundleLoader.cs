b9c5f8b9b6199db56db46ce80991b28611fc329c
stalkerist7474 <80361659+stalkerist7474@users.noreply.github.com>
2025-01-23 21:52:04 +0300

    чистка кода

--- Assets/Main/Scripts/Systems/AssetBundleLoader.cs
+++ Assets/Main/Scripts/Systems/AssetBundleLoader.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
 
--- Assets/Main/Scripts/Systems/MainMenuEntryPoint.cs
+++ Assets/Main/Scripts/Systems/MainMenuEntryPoint.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 //точка входа в сцену в которой настраивается очередность включения игровых объектов/менеджеров
 public class MainMenuEntryPoint : MonoBehaviour
@@ -11,7 +11,7 @@ public class MainMenuEntryPoint : MonoBehaviour
 
     private void Start()
     {
-        foreach (var system in gameSystems) // очередность загрузки/включения систем игры которые будут работать на протяжении всего игрового процесса(savemanager,firebase, реклама, InApp, и другие SDK и системы 
+        foreach (var system in gameSystems) 
         {
             system.Activate();
             Debug.Log($"<color=#20C30C>Load System Main menu = {system.gameObject.name}</color>");
